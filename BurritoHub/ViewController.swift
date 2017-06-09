3bc8cf51c28df2a7f279f9ce9a2ce9c6e158dcf4
Nate Smith <smith.nathan.kyle@gmail.com>
2017-06-08 21:41:58 -0600

    hello world

--- BurritoHub/ViewController.swift
+++ BurritoHub/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        print("Ello World")
+    
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
